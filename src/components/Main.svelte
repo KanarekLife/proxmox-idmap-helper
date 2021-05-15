47379bb99abe8464784e5717f7cc3f3f94508ee0
Stanisław Nieradko <stanislaw@nieradko.com>
2021-05-15 22:25:35 +0200

    remove default rules

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,13 +2,7 @@
     import type Rule from "../data/rule";
     import Input from "./Input.svelte";
     import Output from "./Output.svelte";
-    let rules: Rule[] = [
-        {
-            type: 2,
-            id_in_container: 1002,
-            id_in_host: 1002,
-        },
-    ];
+    let rules: Rule[] = [];
 </script>
 
 <div>
