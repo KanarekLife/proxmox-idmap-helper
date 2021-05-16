282d112551288cb894ccd1bf2ae6e2fba0ddf226
Stanisław Nieradko <stanislaw@nieradko.com>
2021-05-16 13:36:31 +0200

    add link to source code

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,8 +2,11 @@
     <div class="content has-text-centered">
         <p>
             <strong>Proxmox LXC idmap helper</strong> by
-            <a href="https://nieradko.com">Stanisław Nieradko</a>. The source
-            code is released under
+            <a href="https://nieradko.com">Stanisław Nieradko</a>.
+            <a href="https://github.com/KanarekLife/proxmox-idmap-helper"
+                >The source code</a
+            >
+            is released under
             <a href="http://opensource.org/licenses/mit-license.php"
                 >MIT License</a
             >.
